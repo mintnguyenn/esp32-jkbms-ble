16dced6c5cd71e77d40ea329dd2e9e58614eacc5
Minh Nguyen <nhatminh.nguyen.2181998@gmail.com>
2025-03-24 02:46:08 +0700

    Successfully connect to target JK-BMS

--- main/jk_bms_ble.cpp
+++ main/jk_bms_ble.cpp
@@ -1,10 +1,14 @@
 #include "jk_bms_ble.h"
 
+//
+static void ble_connect(void *disc);
+
 static int blecent_gap_event(struct ble_gap_event *event, void *arg)
 {
   // struct ble_gap_conn_desc desc;
   struct ble_hs_adv_fields fields;
   int rc;
+  printf("Event: %d\n", event->type);
 
   switch (event->type) {
   case BLE_GAP_EVENT_DISC:
@@ -13,6 +17,8 @@ static int blecent_gap_event(struct ble_gap_event *event, void *arg)
 
     // Print mac address
     ESP_LOGI(tag, "Device Address: %02x:%02x:%02x:%02x:%02x:%02x", event->disc.addr.val[5], event->disc.addr.val[4], event->disc.addr.val[3], event->disc.addr.val[2], event->disc.addr.val[1], event->disc.addr.val[0]);
+    
+    ble_connect(&event->disc);
     break;
 
   default:
@@ -20,6 +26,85 @@ static int blecent_gap_event(struct ble_gap_event *event, void *arg)
   }
 
   return 0;
+}
+
+/**
+ * Determine if the device is the target device
+ */
+static int is_target_device(const struct ble_gap_disc_desc *disc, const std::string& peer_addr_str)
+{
+    struct ble_hs_adv_fields fields;
+    int rc;
+
+    /* The device has to be advertising connectability. */
+    if (disc->event_type != BLE_HCI_ADV_RPT_EVTYPE_ADV_IND && disc->event_type != BLE_HCI_ADV_RPT_EVTYPE_DIR_IND) {
+        return 0;
+    }
+
+    rc = ble_hs_adv_parse_fields(&fields, disc->data, disc->length_data);
+    if (rc != 0) {
+        return 0;
+    }
+
+    uint8_t peer_addr[6] = {0};
+
+    // Parse MAC address in reverse byte order (BLE style)
+    int parsed = sscanf(peer_addr_str.c_str(), "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+                        &peer_addr[5], &peer_addr[4], &peer_addr[3],
+                        &peer_addr[2], &peer_addr[1], &peer_addr[0]);
+
+    if (parsed != 6) {
+        ESP_LOGE("BLE", "Invalid address format: %s", peer_addr_str.c_str());
+        return 0;
+    }
+
+    // Compare parsed address with discovered device
+    if (memcmp(peer_addr, disc->addr.val, sizeof(disc->addr.val)) != 0) {
+      return 0;
+    }
+
+    return 1;
+}
+
+/**
+ * BLE connect
+ */
+static void ble_connect(void *disc)
+{
+    uint8_t own_addr_type;
+    int rc;
+    ble_addr_t *addr;
+
+    /* Don't do anything if we don't care about this advertiser. */
+    if (!is_target_device((struct ble_gap_disc_desc *)disc, "c8:47:80:1f:05:b6")) {
+        return;
+    }
+
+    /* Scanning must be stopped before a connection can be initiated. */
+    rc = ble_gap_disc_cancel();
+    if (rc != 0) {
+        ESP_LOGE(tag, "Failed to cancel scan; rc=%d\n", rc);
+        return;
+    }
+
+    /* Figure out address to use for connect (no privacy for now) */
+    rc = ble_hs_id_infer_auto(0, &own_addr_type);
+    if (rc != 0) {
+        ESP_LOGE(tag, "error determining address type; rc=%d\n", rc);
+        return;
+    }
+
+    /* Try to connect the the advertiser.  Allow 30 seconds (30000 ms) for
+     * timeout.
+     */
+    addr = &((struct ble_gap_disc_desc *)disc)->addr;
+
+    rc = ble_gap_connect(own_addr_type, addr, 30000, NULL,
+                         blecent_gap_event, NULL);
+    if (rc != 0) {
+        ESP_LOGE(tag, "Error: Failed to connect to device; addr_type=%d; rc=%d\n", addr->type, rc);
+        return;
+    }
 }
 
 void ble_scan(){
