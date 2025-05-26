af15474523f67ac55bb2900cfd3c210102225465
Minh Nguyen <nhatminh.nguyen.2181998@gmail.com>
2025-05-27 00:32:11 +0700

    Move nvs_flash_init() to main task

--- main/ble_manager.cpp
+++ main/ble_manager.cpp
@@ -80,14 +80,6 @@ BleManager::~BleManager() {}
  */
 esp_err_t BleManager::initialize()
 {
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
-
     if (nimble_port_init() != ESP_OK)
     {
         ESP_LOGE(m_tag, "Failed to init NimBLE");
